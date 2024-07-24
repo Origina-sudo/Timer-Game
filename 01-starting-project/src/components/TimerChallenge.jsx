4f39ff54ea030207044a21e7dbccee4ff22b6d43
Origina-sudo <arthurba8585@gmail.com>
2024-07-24 14:04:40 +0000

    changes

--- 01-starting-project/src/components/TimerChallenge.jsx
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -12,6 +12,7 @@ const TimerChallenge = ({title,targetTime}) => {
   return (
     <section className="challenge">
         <h2>{title}</h2>
+        {timerExpired && <p>You Lost</p>}
         <p className="challenge-time">
          {targetTime} second{targetTime > 1 ? "s" :""}
         </p>
