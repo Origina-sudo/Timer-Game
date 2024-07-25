71e4c00aca845ee641a172b5bd322eb455d86577
Origina-sudo <arthurba8585@gmail.com>
2024-07-25 15:00:22 +0000

    Addition

--- 01-starting-project/src/components/ResultModal.jsx
+++ 01-starting-project/src/components/ResultModal.jsx
@@ -15,7 +15,7 @@ const ResultModal = forwardRef(function ResultModal({result,targetTime,remaining
   
     return (
     <dialog ref={dialog} className='result-modal'>
-        <h2>You {result}</h2>
+        {userLost && <h2>You Lost</h2>}
         <p>The Target Time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the Timer with <strong> X seconds left.</strong></p>
         <form method='dialog'>
