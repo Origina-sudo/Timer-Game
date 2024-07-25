92b62603fbc73c7f735ea8bdca2bbae604813b65
Origina-sudo <arthurba8585@gmail.com>
2024-07-25 15:09:14 +0000

    Fixing the You lost Bug

--- 01-starting-project/src/components/ResultModal.jsx
+++ 01-starting-project/src/components/ResultModal.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { forwardRef,useImperativeHandle,useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({result,targetTime,remainingTime},ref){
+const ResultModal = forwardRef(function ResultModal({result,targetTime,remainingTime,onReset},ref){
   const dialog = useRef();
 
   const userLost = remainingTime <= 0;
+const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+
     useImperativeHandle(ref, ()=>{
     return{
         open(){
@@ -17,8 +19,8 @@ const ResultModal = forwardRef(function ResultModal({result,targetTime,remaining
     <dialog ref={dialog} className='result-modal'>
         {userLost && <h2>You Lost</h2>}
         <p>The Target Time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the Timer with <strong> X seconds left.</strong></p>
-        <form method='dialog'>
+        <p>You stopped the Timer with <strong>{formattedRemainingTime}</strong></p>
+        <form method='dialog' onSubmit={onReset}>
             <button>Close</button>
         </form>
     </dialog>
--- 01-starting-project/src/components/TimerChallenge.jsx
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -16,9 +16,13 @@ const TimerChallenge = ({title,targetTime}) => {
 
    if (timeRemaining <= 0){
         clearInterval(timer.current);
-        settimeRemaining(targetTime * 1000);
         dialog.current.open();
    }
+
+  function handleReset(){
+    settimeRemaining(targetTime * 1000);
+  }
+
    function handleStop() {
     dialog.current.open();
           clearInterval(timer.current);
@@ -26,7 +30,7 @@ const TimerChallenge = ({title,targetTime}) => {
   
   return (
     <>  
-      <ResultModal ref ={dialog} targetTime={targetTime} remainingTime ={timeRemaining}/>
+      <ResultModal ref ={dialog} targetTime={targetTime} remainingTime ={timeRemaining} onReset={handleReset}/>
       <section className="challenge">
         <h2>{title}</h2>
 
